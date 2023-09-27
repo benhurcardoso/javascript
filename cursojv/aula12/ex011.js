955eeafc65135fe5b35bfa4cdd050a15c4570e9e
Ben-hur Cardoso Mazarem <131083420+benhurcardoso@users.noreply.github.com>
2023-09-26 21:26:19 -0300

    aula 12, ex011, ex012 e ex013

--- cursojv/aula12/ex011.js
+++ cursojv/aula12/ex011.js
@@ -1,6 +1,9 @@
-var idade = 22
+var idade = 67
+console.log(`Você tem ${idade} anos.`)
 if (idade < 18) {
     console.log('Menor de idade')
+} else if (idade < 18 || idade > 65) {
+    console.log('Voto opcional')
 } else {
-    console.log('Maior de idade')
+    console.log('Voto obrigatório')
 }
